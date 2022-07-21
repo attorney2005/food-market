f3dd8e2a4d91b5b9211aac20f7b0296a2d3f0767
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-07-21 09:21:16 +0300

    Начал делать компонент без картинки

--- my-app/src/components/menu-component/menu-component.tsx
+++ my-app/src/components/menu-component/menu-component.tsx
@@ -8,20 +8,30 @@ type OfferWithPhotoSmallProps = {
   text: string;
 }
 
-function MenuComponent(props:OfferWithPhotoSmallProps): JSX.Element {
-  return (
-    <div className="menu__component">
-      <div className="offer__container-photo">
-        <img className="menu__component-photo" src="../../header-pictures/pic.jpg"
-             width="252" height="192" alt="Edges">
-        </img>
+function MenuComponent(props: OfferWithPhotoSmallProps): JSX.Element {
+  if (props.id < 5) {
+    return (
+      <div className="menu__component-with-out-photo">
+          <h4 className="menu__component-title">{props.title}</h4>
+          <p className="menu__component-text">{props.text}</p>
       </div>
-      <div className="menu__component-text-block">
-        <h4 className="menu__component-title">{props.title}</h4>
-        <p className="menu__component-text">{props.text}</p>
-      </div>
-    </div>
-  )
-}
+    )
+  } else {
 
-export default MenuComponent
+    return (
+      <div className="menu__component">
+        <div className="offer__container-photo">
+          <img className="menu__component-photo" src={props.img}
+               width="252" height="192" alt="Edges">
+          </img>
+        </div>
+        <div className="menu__component-text-block">
+          <h4 className="menu__component-title">{props.title}</h4>
+          <p className="menu__component-text">{props.text}</p>
+        </div>
+      </div>
+    )
+  }
+        }
+
+        export default MenuComponent
