4ca5523997b82f095fbac0f81335e2d088dda2ed
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-07-24 13:28:54 +0300

    Refactoring

--- my-app/src/components/menu-component/menu-component.tsx
+++ my-app/src/components/menu-component/menu-component.tsx
@@ -6,20 +6,28 @@ type OfferWithPhotoSmallProps = {
   img: string;
   title: string;
   text: string;
+  like: number,
+  time: string,
+  calories: string,
 }
 
 function MenuComponent(props: OfferWithPhotoSmallProps): JSX.Element {
   if (props.id < 5) {
     return (
-      <div className="menu__component-with-out-photo">
+        <div className="menu__component-with-out-photo">
           <h4 className="menu__component-title">{props.title}</h4>
           <p className="menu__component-text">{props.text}</p>
-      </div>
+          <div className="menu__component-with-out-photo-info-container">
+            <span className="menu__component-info">{props.like}</span>
+            <span className="menu__component-info">{props.time}</span>
+            <span className="menu__component-info">{props.calories}</span>
+          </div>
+        </div>
     )
-  } else {
-
+  }
+  if (props.id > 7) {
     return (
-      <div className="menu__component">
+      <div className="menu__component-with-small-photo">
         <div className="offer__container-photo">
           <img className="menu__component-photo" src={props.img}
                width="252" height="192" alt="Edges">
@@ -28,10 +36,34 @@ function MenuComponent(props: OfferWithPhotoSmallProps): JSX.Element {
         <div className="menu__component-text-block">
           <h4 className="menu__component-title">{props.title}</h4>
           <p className="menu__component-text">{props.text}</p>
+          <div className="menu__component-with-out-photo-info-container">
+            <span className="menu__component-info">{props.like}</span>
+            <span className="menu__component-info">{props.time}</span>
+            <span className="menu__component-info">{props.calories}</span>
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="menu__component">
+        <div className="offer__container-photo">
+          <img className="menu__component-photo" src={props.img}
+               width="534" height="192" alt="Edges">
+          </img>
+        </div>
+        <div className="menu__component-text-block">
+          <h4 className="menu__component-title">{props.title}</h4>
+          <p className="menu__component-text">{props.text}</p>
+          <div className="menu__component-with-out-photo-info-container">
+            <span className="menu__component-info">{props.like}</span>
+            <span className="menu__component-info">{props.time}</span>
+            <span className="menu__component-info">{props.calories}</span>
+          </div>
         </div>
       </div>
     )
   }
-        }
+}
 
-        export default MenuComponent
+export default MenuComponent
