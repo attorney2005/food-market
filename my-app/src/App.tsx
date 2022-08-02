bc1ee5e7bb06d407cd5f8223a72390b972cf1809
attorney2005 <72994919+attorney2005@users.noreply.github.com>
2022-08-02 14:44:48 +0300

    Начал делать футер

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import SortTeg from "./components/sort-teg/sort-teg";
 import Header from "./components/header/header";
 import MenuContainer from "./components/menu-container/menu-container";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <MenuContainer/>
+      <Footer/>
     </div>
   );
 }
